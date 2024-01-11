bcc579e879569051dd1a981782c6ad8d8b2c5459
EndrexAkoto <martinakoto3@gmail.com>
2024-01-11 13:13:20 -0800

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python3
+"""Unlock boxes"""
+
 
 def canUnlockAll(boxes):
+    """Function which checks if it can unlock all the boxes
     """
-    Determines if all the boxes can be opened.
 
-    Args:
-    - boxes (List[List[int]]): A list of lists where each inner list represents a box and contains keys to other boxes.
-
-    Returns:
-    - bool: True if all boxes can be opened, else False.
-    """
-    if not boxes:
-        return False
-
-    num_boxes = len(boxes)
-    keys = {0}
-    visited = set()
-
-    while keys:
-        current_key = keys.pop()
-        visited.add(current_key)
-
-        if current_key < num_boxes:
-            keys.update(key for key in boxes[current_key] if key not in visited)
-
-    return len(visited) == num_boxes
-
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))
-
+    x = [0]
+    for a in x:
+        for boxKey in boxes[a]:
+            if boxKey not in x and boxKey < len(boxes):
+                x.append(boxKey)
+    if len(x) == len(boxes):
+        return True
+    return False
