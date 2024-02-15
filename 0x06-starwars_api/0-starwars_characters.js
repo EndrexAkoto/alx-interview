3bb65fa6b2649c7a0b24e828437cebd67ca5afa4
Endrex Akoto <martinakoto3@gmail.com>
2024-02-15 22:44:35 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,40 +2,27 @@
 
 const request = require('request');
 
-// Get the movie ID from command line arguments
 const movieID = process.argv[2];
+const options = { json: true };
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-// Define the URL for the Star Wars API films endpoint
-const apiUrl = `https://swapi.dev/api/films/${movieID}/`;
-
-// Make a GET request to the API
-request(apiUrl, (error, response, body) => {
+request(url, options, async (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-  } else {
-    // Parse the JSON response
-    const filmData = JSON.parse(body);
+    console.error(error);
+    return;
+  }
 
-    // Extract the characters array from the response
-    const characters = filmData.characters;
-
-    // Print each character name
-    characters.forEach(characterUrl => {
-      // Make a GET request to the character URL
-      request(characterUrl, (error, response, body) => {
+  for (const character of body.characters) {
+    await new Promise((resolve) => {
+      request(character, options, (error, response, body) => {
         if (error) {
-          console.error('Error:', error);
-        } else if (response.statusCode !== 200) {
-          console.error('Unexpected status code:', response.statusCode);
-        } else {
-          // Parse the JSON response
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
+          console.error(error);
+          resolve();
         }
+
+        console.log(body.name);
+        resolve();
       });
     });
   }
 });
-
